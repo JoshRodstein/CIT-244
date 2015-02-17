292f2fbdba094c87021a4a774feca09c3e5a782e
JazzyDragon <joshuarodstein@gmail.com>
2015-02-16 19:46:48 -0500

    first commit

---
+++ LabAssignment3.java
@@ -0,0 +1,26 @@
+
+import java.util.Random;
+
+
+/**
+ * Lab Assignment 3
+ * @author joshua.rodstein
+ * 2/6/15
+ */
+public class LabAssignment3 {
+    
+   
+    public static void main(String[] args) {
+        Random numGen = new Random();
+        int num1 = numGen.nextInt(50) + 1, num2 = numGen.nextInt(50) + 1;
+        int largerNum = Math.max(num1, num2);
+    
+        System.out.println("The numbers are " + num1 + " and " + num2 +
+                           "\nThe larger number is " + largerNum );
+        
+        
+        
+    }
+    
+}
+
